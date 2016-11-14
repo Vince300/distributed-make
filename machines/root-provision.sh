f649227879cb587befa9fd940f137e29dbe6247a
Vincent Tavernier <vincent.tavernier@ensimag.grenoble-inp.fr>
2016-11-14 01:22:48 +0100

    Updated provisioning script for matrix example

--- machines/root-provision.sh
+++ machines/root-provision.sh
@@ -170,3 +170,203 @@ int main(int argc, char **argv) {
 else
   echo "premier is already installed, skipping."
 fi
+
+# Install matrix multiplication tools
+tee /usr/local/bin/matfuse <<\FUSE
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my ($out, $sx, $sy) = splice(@ARGV, 0, 3);
+
+my @C;
+my $current_x = 0;
+my $last_x = 0;
+my $current_y = 0;
+my $matrix_count = 0;
+
+my $file;
+foreach $file (@ARGV) {
+	open(IN, " < $file");
+	my $header = <IN>;
+	my @line;
+	my $count = 0;
+	$matrix_count++;
+	while(<IN>) {
+		chomp();
+		@line = split / /;
+		#put at right place
+		$C[$current_x] = [] unless defined $C[$current_x];
+		$C[$current_x][$current_y+$_] = $line[$_] foreach (0..$#line);
+		$current_x++;
+		$count++;
+	}
+	close(IN);
+	if ($matrix_count == $sy) {
+		$matrix_count = 0;
+		$last_x += $count;
+		$current_y = 0;
+	} else {
+		$current_x = $last_x;
+		$current_y += scalar @line;
+	}
+}
+
+open(OUT, "> $out");
+my $m = scalar @C;
+my $n = scalar @{$C[0]};
+print OUT "$m $n\n";
+foreach(@C) {
+	print OUT join(" ", @{$_})."\n";
+}
+close(OUT);
+FUSE
+chmod +x /usr/local/bin/matfuse
+echo "installed matfuse to /usr/local/bin/matfuse."
+
+tee /usr/local/bin/matmultiply <<\MULTIPLY
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my ($out, $in1, $in2) = @ARGV;
+
+open(IN1, " < $in1");
+open(IN2, " < $in2");
+my $header = <IN1>;
+chomp($header);
+my ($m, $n) = split(/ /, $header);
+$header = <IN2>;
+chomp($header);
+my($n2, $o) = split(/ /, $header);
+die "error in sizes" unless $n == $n2;
+my @A;
+while(<IN1>) {
+	chomp();
+	my @line = split / /;
+	push @A, [@line];
+}
+close(IN1);
+my @B;
+while(<IN2>) {
+	chomp();
+	my @line = split / /;
+	push @B, [@line];
+}
+close(IN2);
+
+my @C;
+
+my ($i, $j, $k);
+foreach $i (0..($m-1)) {
+	push @C, [];
+	foreach $j (0..($o-1)) {
+		my $sum = 0;
+		foreach $k (0..($n-1)) {
+			$sum += $A[$i][$k] * $B[$k][$j];
+		}
+		$C[$i][$j] = $sum;
+	}
+}
+
+open(OUT, "> $out");
+print OUT "$m $o\n";
+foreach(@C) {
+	print OUT join(" ", @{$_})."\n";
+}
+close(OUT);
+MULTIPLY
+chmod +x /usr/local/bin/matmultiply
+echo "installed matmultiply to /usr/local/bin/matmultiply."
+
+tee /usr/local/bin/matsplit <<\SPLIT
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my ($output_file, $input_file, $sx, $sy, $x, $y) = @ARGV;
+
+die "parameters missing" unless defined $y;
+
+open(FILE, "< $input_file");
+my $header = <FILE>;
+my ($w, $h) = split(/ /, $header);
+my $bw = $w / $sx;
+my $bh = $h / $sy;
+my $start_x = $bw * ($x-1);
+my $end_x = $bw * $x;
+my $start_y = $bh * ($y-1);
+
+open(DFILE, "> $output_file");
+print DFILE "$bw $bh\n";
+
+my $count = 0;
+while(<FILE>) {
+	chomp();
+	last if $count >= $end_x;
+	if ($count >= $start_x) {
+		my @line = split / /;
+		my @selection = splice(@line, $start_y, $bh);
+		print DFILE join(" ", @selection)."\n";
+	}
+	$count++;
+}
+
+close(FILE);
+close(DFILE);
+SPLIT
+chmod +x /usr/local/bin/matsplit
+echo "installed matsplit to /usr/local/bin/matsplit."
+
+tee /usr/local/bin/matsum <<\SUM
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my ($out, @in) = @ARGV;
+my ($m, $n);
+my @A_in=();
+my $size=0;
+foreach (@in) {
+	open(IN, " < $_");
+	my $header = <IN>;
+	chomp($header);
+	my ($m1, $n1) = split(/ /, $header);
+  ($m, $n)=($m1, $n1) unless defined $m and defined $n;
+	die "error in sizes" unless $n == $n1 and $m == $m1;
+  my @tmp;
+	while(<IN>) {
+	  chomp();
+	  my @line = split / /;
+	  push @tmp, [@line];
+	}
+	close(IN);
+  $A_in[$size]=[@tmp];
+  $size++;
+}
+	my @C;
+
+	my ($i, $j);
+	foreach $i (0..($m-1)) {
+	  push @C, [];
+	  foreach $j (0..($n-1)) {
+      my $k;
+      for($k=0;$k<$size;$k++){
+        $C[$i][$j] += $A_in[$k][$i][$j];
+      }
+	  }
+	}
+
+	open(OUT, "> $out");
+	print OUT "$m $n\n";
+	foreach(@C) {
+	  print OUT join(" ", @{$_})."\n";
+	}
+	close(OUT);
+SUM
+chmod +x /usr/local/bin/matsum
+echo "installed matsum to /usr/local/bin/matsum."
